93b7ffb4d6091c38f2d654e1d1eb2cd332178756
yousour <hengyousour@gmail.com>
2015-11-15 10:36:07 +0700

    correct interface all Invoice

--- dental/server/methods/reports/purchase.js
+++ dental/server/methods/reports/purchase.js
@@ -1,53 +1,69 @@
 Meteor.methods({
-    dental_purchase: function (params) {
-        var self = params;
-        var data = {
-            title: {},
-            header: {},
-            content: [],
-            footer: {}
-        };
+  dental_purchase: function(params) {
+    var self = params;
+    var data = {
+      title: {},
+      header: {},
+      content: [],
+      footer: {}
+    };
 
-        /********* Title *********/
-        var company = Cpanel.Collection.Company.findOne();
-        data.title = {
-            company: company
-        };
+    /********* Title *********/
+    var company = Cpanel.Collection.Company.findOne();
+    data.title = {
+      company: company
+    };
 
-        /********* Header ********/
-        var purchaseDoc = Dental.Collection.Purchase.findOne({_id:self.purchase});
-        var exchange = Cpanel.Collection.Exchange.findOne({dateTime:{$lte:moment().format("YYYY-MM-DD HH:mm:ss")}},{sort: {dateTime: -1}});
-        data.header = purchaseDoc;
-        data.header.exchange = numeral(exchange.rates.USD).format('$ 0,0.00') +" | "+ numeral(exchange.rates.KHR).format('0,0.00')+" R" + " | "+ numeral(exchange.rates.THB).format('0,0.00')+" B";
+    /********* Header ********/
+    var purchaseDoc = Dental.Collection.Purchase.findOne({
+      _id: self.purchase
+    });
+    var exchange = Cpanel.Collection.Exchange.findOne({
+      dateTime: {
+        $lte: moment().format("YYYY-MM-DD HH:mm:ss")
+      }
+    }, {
+      sort: {
+        dateTime: -1
+      }
+    });
+    data.header = purchaseDoc;
+    data.header.exchange = numeral(exchange.rates.USD).format('$ 0,0.00') +
+      " | " + numeral(exchange.rates.KHR).format('0,0.00') + " R" + " | " +
+      numeral(exchange.rates.THB).format('0,0.00') + " B";
 
-        /********** Content & Footer **********/
-        var content = [];
+    /********** Content & Footer **********/
+    var content = [];
 
-        // Each item
-        var index = 1;
-        _.each(purchaseDoc.items, function (obj) {
-            var itemDoc = Dental.Collection.OrderItem.findOne(obj.orderItemId);
-            obj.index = index;
-            obj.itemName = itemDoc.name;
-            obj.price = numeral(obj.price).format('0,0.00');
-            obj.amount = numeral(obj.amount).format('0,0.00');
+    // Each item
+    var index = 1;
+    _.each(purchaseDoc.items, function(obj) {
+      var itemDoc = Dental.Collection.OrderItem.findOne(obj.orderItemId);
+      obj.index = index;
+      obj.itemName = itemDoc.name;
+      obj.price = numeral(obj.price).format('$0,0.00');
+      obj.amount = numeral(obj.amount).format('$0,0.00');
 
-            content.push(obj);
+      content.push(obj);
 
-            index += 1;
-        });
+      index += 1;
+    });
 
-        data.footer.total = numeral(purchaseDoc.total).format('$0,0.00');
-        data.footer.totalKhr = "R"+numeral(purchaseDoc.total*exchange.rates.KHR).format('0,0.00');
-        data.footer.totalThb = "THB"+numeral(purchaseDoc.total*exchange.rates.THB).format('0,0.00');
+    data.footer.total = numeral(purchaseDoc.total).format('$0,0.00');
+    data.footer.totalKhr = "R" + numeral(purchaseDoc.total * exchange.rates
+      .KHR).format('0,0.00');
+    data.footer.totalThb = "THB" + numeral(purchaseDoc.total * exchange.rates
+      .THB).format('0,0.00');
 
-        if (content.length > 0) {
-            data.content = content;
+    if (content.length > 0) {
+      data.content = content;
 
-            return data;
-        } else {
-            data.content.push({index: 'no results'});
-            return data;
-        }
+      return data;
+    } else {
+      data.content.push({
+        index: 'no results'
+      });
+      return data;
     }
+  }
 });
